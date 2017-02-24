10977f51e38bbf341f43567da83a9891e4cdb7be
Khalids1066 <khalid.saeed1066@gmail.com>
2017-02-24 17:33:20 +1300

    init commit
    nabber,  4 hidden divs acting as “pages”
    using google api and Facebook plugins

---
+++ js/script.js
@@ -0,0 +1,46 @@
+$(document).ready(function(){
+    $("#homeBtn").click(function(){
+    	$("#aboutDiv").addClass('hidden');
+    	$("#galleryDiv").addClass('hidden');
+    	$("#contactDiv").addClass('hidden');
+    	$("#homeDiv").removeClass('hidden');
+    });
+
+    $("#aboutBtn").click(function(){
+    	$("#homeDiv").addClass('hidden');
+    	$("#galleryDiv").addClass('hidden');
+    	$("#contactDiv").addClass('hidden');
+    	$("#aboutDiv").removeClass('hidden');
+    	initMap();
+    	google.maps.event.trigger(map, 'resize');
+    	
+    });
+
+    $("#galleryBtn").click(function(){
+    	$("#aboutDiv").addClass('hidden');
+    	$("#homeDiv").addClass('hidden');
+    	$("#contactDiv").addClass('hidden');
+    	$("#galleryDiv").removeClass('hidden');
+    });
+
+    $("#contactBtn").click(function(){
+    	$("#aboutDiv").addClass('hidden');
+    	$("#galleryDiv").addClass('hidden');
+    	$("#homeDiv").addClass('hidden');
+    	$("#contactDiv").removeClass('hidden');
+    });
+
+});
+
+function initMap() {
+	var location = {lat: -36.903190, lng: 174.688691};
+	var map = new google.maps.Map(document.getElementById('map'), {
+	  zoom: 12,
+	  center: location
+	});
+
+	var marker = new google.maps.Marker({
+	  position: location,
+	  map: map
+	});
+}
